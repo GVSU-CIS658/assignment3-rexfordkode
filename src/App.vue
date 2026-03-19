128f257d175af621f7e829cfb46e2004cc9996bb
Rexfordkode <rexfordk.heatmap@gmail.com>
2026-03-19 01:20:40 -0400

    feat: add radio button selections for base, creamer, and syrup options in App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,50 @@
       <li>
         <template v-for="temp in temps" :key="temp">
           <label>
-            <input
-              type="radio"
-              name="temperature"
-              :id="`r${temp}`"
-              :value="temp"
-              v-model="currentTemp"
-            />
+            <input type="radio" name="temperature" :id="`r${temp}`" :value="temp" v-model="currentTemp" />
             {{ temp }}
           </label>
         </template>
+
       </li>
+      <li>
+        <template v-for="base in bases" :key="base.id">
+          <label>
+            <input type="radio" name="base" :id="`${base.id}`" :value="base.name" v-model="selectedBase" />
+            {{ base.name }}
+          </label>
+        </template>
+      </li>
+      <li>
+        <template v-for="creamer in creamers" :key="creamer.id">
+          <label>
+            <input type="radio" name="creamer" :id="`r${creamer.id}`" :value="creamer.name" v-model="selectedCreamer" />
+            {{ creamer.name }}
+          </label>
+        </template>
+      </li>
+      <li>
+        <template v-for="syrup in syrups" :key="syrup.id">
+          <label>
+            <input type="radio" name="syrup" :id="`${syrup.id}`" :value="syrup.name" v-model="selectedSyrup" />
+            {{ syrup.name }}
+          </label>
+        </template>
+      </li>
+
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
-import { temps, currentTemp } from "./stores/beverage";
+import Syrup from "./components/Syrup.vue";
+import { temps, currentTemp, bases, creamers, syrups } from "./stores/beverage";
+
+const selectedBase = ref(bases.value[1].name);
+const selectedCreamer = ref(creamers.value[3].name);
+const selectedSyrup = ref(syrups.value[3].name);
 </script>
 
 <style lang="scss">
@@ -36,6 +62,7 @@ html {
   background-color: #6e4228;
   background: linear-gradient(to bottom, #6e4228 0%, #956f5a 100%);
 }
+
 ul {
   list-style: none;
 }
